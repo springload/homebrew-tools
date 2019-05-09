af9c4cf0229aeb59f666a63744b39041600b7fd0
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-10 10:45:13 +1200

    Brew formula update for aws-ssh version v0.5

--- Formula/aws-ssh.rb
+++ Formula/aws-ssh.rb
@@ -1,9 +1,10 @@
+require_relative "../custom_download_strategy"
 class AwsSsh < Formula
   desc ""
   homepage ""
-  url "https://github.com/springload/aws-ssh/releases/download/v0.4/aws-ssh_0.4_darwin_amd64.tar.gz"
-  version "0.4"
-  sha256 "cd9ead2ee4037d9e4ec3780055006edfca7c8615418e5855b469aa9288029567"
+  url "https://github.com/springload/aws-ssh/releases/download/v0.5/aws-ssh_0.5_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.5"
+  sha256 "f78840cabe89aaf2309a533e9555f19f1141b37f32c250171221602aa22c1747"
 
   def install
     bin.install "aws-ssh"
