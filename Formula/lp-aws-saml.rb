99483388feab7fb154715810bbd82bd1c8ac6425
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-10 10:24:08 +1200

    Brew formula update for lp-aws-saml version v0.4

--- Formula/lp-aws-saml.rb
+++ Formula/lp-aws-saml.rb
@@ -1,9 +1,9 @@
 class LpAwsSaml < Formula
   desc "Temporary Credentials for AWS CLI for LastPass SAML login"
   homepage "https://github.com/springload/lp-aws-saml"
-  url "https://github.com/springload/lp-aws-saml/releases/download/v0.3/lp-aws-saml_0.3_darwin_amd64.tar.gz"
-  version "0.3"
-  sha256 "c3ca242efd7f41f85c2c68272aa42c067382879e430018c537b6ea4e882f4c11"
+  url "https://github.com/springload/lp-aws-saml/releases/download/v0.4/lp-aws-saml_0.4_darwin_amd64.tar.gz"
+  version "0.4"
+  sha256 "d7c5783455896880a5ae619908914d585e0ff3a50de91fdba0ebb43ae3c60692"
 
   def install
     bin.install "lp-aws-saml"
