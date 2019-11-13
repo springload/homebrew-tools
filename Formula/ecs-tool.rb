2b84a2ee7af4a359651166032c4e0c61691f16a9
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-14 11:05:07 +1300

    Brew formula update for ecs-tool version 1.3.1

--- Formula/ecs-tool.rb
+++ Formula/ecs-tool.rb
@@ -1,9 +1,9 @@
 class EcsTool < Formula
   desc ""
   homepage ""
-  url "https://github.com/springload/ecs-tool/releases/download/1.3/ecs-tool_1.3_darwin_amd64.tar.gz"
-  version "1.3"
-  sha256 "6c709d6dfe8d539ae54fbcc9f611458312da0624010e555f7e923a72e739a607"
+  url "https://github.com/springload/ecs-tool/releases/download/1.3.1/ecs-tool_1.3.1_darwin_amd64.tar.gz"
+  version "1.3.1"
+  sha256 "df191e8f2a54cdc66fb83c8e1c885b50b98695ed0d5c02fb82584b619c2d2d96"
 
   def install
     bin.install "ecs-tool"
