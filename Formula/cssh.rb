0bf5f64544473a1ed27e03bf97bb1154b86fc4e9
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-20 20:12:27 +1300

    Brew formula update for cssh version v0.0.2

--- Formula/cssh.rb
+++ Formula/cssh.rb
@@ -1,9 +1,10 @@
+require_relative "../custom_download_strategy"
 class Cssh < Formula
   desc ""
   homepage ""
-  url "https://github.com/springload/cssh/releases/download/v0.0.1/cssh_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "565dff7c84c970f330b6efc7114836b50103ee5ca40df9afe81705c522a4130b"
+  url "https://github.com/springload/cssh/releases/download/v0.0.2/cssh_0.0.2_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.0.2"
+  sha256 "f0e319ac0768bcaf9c68bdad0bc057583dba07be98e0c8095cad6181c4f45959"
 
   def install
     bin.install "cssh"
