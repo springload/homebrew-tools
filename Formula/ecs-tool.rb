9ddefed3eb6da5ae492196b438f07c8d5b888b4c
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-29 13:48:35 +1300

    Brew formula update for ecs-tool version 1.6.1

--- Formula/ecs-tool.rb
+++ Formula/ecs-tool.rb
@@ -2,16 +2,16 @@
 class EcsTool < Formula
   desc ""
   homepage ""
-  version "1.6"
+  version "1.6.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/springload/ecs-tool/releases/download/1.6/ecs-tool_1.6_darwin_amd64.tar.gz"
-    sha256 "7936c1aefa079700115c6ad53526c2587babd0f5b3e30119dc18a742f08ba072"
+    url "https://github.com/springload/ecs-tool/releases/download/1.6.1/ecs-tool_1.6.1_darwin_amd64.tar.gz"
+    sha256 "ac7b09df933d80dd1288575d67d1045af49c84a984062f2e759ec64755e2a5e3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/springload/ecs-tool/releases/download/1.6/ecs-tool_1.6_linux_amd64.tar.gz"
-      sha256 "27702a0d3da057cd3c3e7596782f9e03bfc16ea6cf24c026e2d0c5e915093c9b"
+      url "https://github.com/springload/ecs-tool/releases/download/1.6.1/ecs-tool_1.6.1_linux_amd64.tar.gz"
+      sha256 "1598a77e1b8b6ed8aa8b45f6b8d1258c93ff331b1bdca85f394eaf5fbb0fe92c"
     end
   end
 
