2d662b98c5a2b95ac7d02433d5d256a5b6f189fb
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-08 12:45:41 +1300

    Brew formula update for aws-ssh version v2.2.0

--- Formula/aws-ssh.rb
+++ Formula/aws-ssh.rb
@@ -5,20 +5,20 @@
 class AwsSsh < Formula
   desc ""
   homepage ""
-  version "2.1.4"
+  version "2.2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/springload/aws-ssh/releases/download/v2.1.4/aws-ssh_2.1.4_darwin_amd64.tar.gz"
-      sha256 "d657612e64a522ee684da75d3b6f50acf87f854818a484d28110b7b80db7a12d"
+      url "https://github.com/springload/aws-ssh/releases/download/v2.2.0/aws-ssh_2.2.0_darwin_amd64.tar.gz"
+      sha256 "8f2cfc78604fa510a1de38d76cf39c4e32573da4ae604a0fa3ede1041a8adc49"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/springload/aws-ssh/releases/download/v2.1.4/aws-ssh_2.1.4_linux_amd64.tar.gz"
-      sha256 "4f6c734e81cc36a3fa322a3e906dccc9ee8c6367501c1996513cc5a7e05d4099"
+      url "https://github.com/springload/aws-ssh/releases/download/v2.2.0/aws-ssh_2.2.0_linux_amd64.tar.gz"
+      sha256 "6754e170c94ab533618c46cb8dd92670abf3a682f176b3e01f21e84a9ec35974"
     end
   end
 
